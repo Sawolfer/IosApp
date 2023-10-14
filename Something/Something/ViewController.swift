7b829348e358171e57653349af6db51aa7e9520b
Sawolf <Savipon@yandex.ru>
2023-10-14 16:49:08 +0300

    nu tak nemnogo podelal

--- Something/Something/ViewController.swift
+++ Something/Something/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var BRPMLabel: UILabel!
+    @IBOutlet var getSomethingInfo: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getSomethingInfo.addTarget(self, action: #selector(SwitchText), for: .touchUpInside)
         // Do any additional setup after loading the view.
     }
 
-
+    @objc func SwitchText(){
+//        BRPMLabel.text = "ладно"
+        let urlString = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true"
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) {data, response, error in
+            print(data)
+        }
+        task.resume()
+    }
 }
 
